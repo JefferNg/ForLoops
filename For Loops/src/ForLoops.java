5a63ae84f9e0729386c884c76eec9629cb852d7e
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2018-08-27 08:45:52 -0600

    Finished challenge two

---
+++ For Loops/src/ForLoops.java
@@ -0,0 +1,80 @@
+
+public class ForLoops
+	{
+
+		public static void main(String[] args)
+			{
+				challengeOne();
+				challengeTwo();
+				challengeThree();
+				challengeFour();
+				challengeFive();
+				challengeSix();
+				challengeSeven();
+				challengeEight();			
+
+			}
+
+		private static void challengeOne()
+			{
+				
+				for(int i = 1; i <= 5; i++)
+					{
+						System.out.println(i);
+					}
+				
+				
+				
+				
+			}
+
+		private static void challengeTwo()
+			{
+				
+				for(int i = 2; i <= 10; i = i + 2)
+					{
+						System.out.println(i + ". I Love Mullen");
+					}
+				
+				
+				
+				
+			}
+
+		private static void challengeThree()
+			{
+				
+				
+			}
+
+		private static void challengeFour()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+		private static void challengeFive()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+		private static void challengeSix()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+		private static void challengeSeven()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+		private static void challengeEight()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+	}
